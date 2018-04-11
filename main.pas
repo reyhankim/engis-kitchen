77ab140124e04c3464e4d1444c0c31b3b3d91fcb
Reyhan Kim <reyhan_kim@icloud.com>
2018-04-11 19:24:56 +0700

    Update main, tipe

--- main.pas
+++ main.pas
@@ -1,23 +1,12 @@
 // file main
 program main;
 uses tipe, handler, aksiLoad;
-
-var
-    arrBahanMentah : array [1..1024] of bahan_mentah;
-    arrBahanOlahan : array [1..1024] of hasil_olah;
-    dataSimulasi : array [1..128] of data;
-    arrInvMentah : array [1..512] of isi_inventori;
-    arrInvOlahan : array [1..512] of isi_inventori;
-    arrResep : array [1..256] of hasil_olah;
-    currentSimulasi : data;
-    uang : longint;
-    terminateProgram, simulasiRunning : boolean;
-    todayIstirahatCount, todayMakanCount : integer;
     
 begin
+    //load seluruh data dari file eksternal
     load();
-    terminateProgram := False;
-    while(not(terminateProgram)) do begin
+    terminateProgram := False; 
+    while (not(terminateProgram)) do begin //jalankan program hingga exit()
         runUserInterface();
     end;
 end.
