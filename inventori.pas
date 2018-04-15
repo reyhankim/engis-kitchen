9855c75fd27134226229866b5ab8e038531842a2
Firastic <adyaksa@students.itb.ac.id>
2018-04-15 15:59:46 +0700

    Add inventori

---
+++ inventori.pas
@@ -0,0 +1,44 @@
+unit inventori;
+//useInventori sama freeInventori pake var?
+interface
+	procedure useInventori(plusInventori: integer); 
+	procedure freeInventori(minusInventori: integer); 
+	procedure lihatInventori();
+	procedure upgradeInventori();
+implementation
+	uses tipe;
+	procedure useInventori(plusInventori: integer); 
+	begin
+		currentSimulasi.inven_used += plusInventori;
+	end;
+	procedure freeInventori(minusInventori: integer);
+	begin
+		currentSimulasi.inven_used -= minusInventori;
+	end;
+
+	procedure lihatInventoriTemplate(var arrInvTemplate: array of isi_inventori);
+	var
+		curIdx: Integer;
+
+	begin
+		//sort(arrInvTemplate);
+		curIdx := 1;
+		while(arrInvTemplate[curIdx].nama <> '') do
+		begin
+			writeln('Nama: ', arrInvTemplate[curIdx].nama);
+			writeln('Kadaluarsa: ', arrInvTemplate[curIdx].dd,'/',arrInvTemplate[curIdx].mm,'/',arrInvTemplate[curIdx].yy);
+			writeln('Jumlah: ', arrInvTemplate[curIdx].jumlah);
+		end;
+	end;
+
+	procedure lihatInventori();
+	begin
+		lihatInventoriTemplate(arrInvMentah);
+		lihatInventoriTemplate(arrInvOlahan);
+	end;
+	procedure upgradeInventori();
+	begin
+		//Harga: 100000
+		currentSimulasi.inven_cap += 25;
+	end;
+end.
