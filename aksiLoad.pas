b618e310e1fbe8b81288392718722553f9eb82b0
Firastic <adyaksa@students.itb.ac.id>
2018-04-15 14:27:15 +0700

    Untested aksiLoad

--- aksiLoad.pas
+++ aksiLoad.pas
@@ -2,13 +2,12 @@ unit aksiLoad;
 
 interface
     uses tipe;
-    procedure loadBahanMentah(var arrBahanMentah : array of tipe.bahan_mentah); 
-    procedure loadBahanOlahan(var arrBahanOlahan : array of tipe.hasil_olah);
-    procedure loadResep(var arrResep : array of tipe.hasil_olah); 
-    procedure loadInventori(var arrInvMentah : array of tipe.isi_inventori; var arrInvOlahan : array of tipe.isi_inventori); 
+    procedure loadBahanMentah(); 
+    procedure loadBahanOlahan();
+    procedure loadResep(); 
+    procedure loadInventori(); 
+    procedure loadSimulasi();
 implementation
-    //uses tipe;
-
     type //tipe data untuk mengambil hasil parsing dari suatu string menjadi array of string
         parseResult = record
             arr : array[1..105] of string;
@@ -28,7 +27,7 @@ implementation
         cur := 1;
         for i := 1 to length(s) do 
         begin
-            if((s[i] = '|') or (i = length(s)))then
+            if((s[i] = '|') or (i = length(s)) or (s[i] = '/'))then
             begin
                 while (input.arr[cur][1] = ' ') do //Menghapus semua spasi di awal string
                 begin
@@ -52,19 +51,11 @@ implementation
     //Mengubah suatu string menjadi longint dengan nilai positif
     //I.S: convStringToInt nilainya 0
     //F.S: convStringToInt bernilai s namun dalam bentuk longint
-    var
-        i: integer;
-    
     begin
-        convStringToInt := 0;
-        for i := 1 to length(s) do
-        begin
-            convStringToInt *= 10;
-            convStringToInt += ord(s[i]);
-        end;
+        val(s, convStringToInt)
     end;
 
-    procedure loadBahanMentah(var arrBahanMentah : array of tipe.bahan_mentah); 
+    procedure loadBahanMentah(); 
     //Membaca file dari 'listBahanMentah.dat' dan memasukannya ke dalam array
     //I.S: arrBahanMentah kosong
     //F.S: arrBahanMentah terisi
@@ -77,6 +68,7 @@ implementation
         idx: integer;
     
     begin
+        idx := 1;
         Assign(fileInput, FILENAME);
         reset(fileInput);
         while not eof(fileInput) do //membaca sampai end of file
@@ -100,6 +92,7 @@ implementation
         idx,i: integer;
     
     begin
+        idx := 1;
         Assign(fileInput, FILENAME);
         reset(fileInput);
         while not eof(fileInput) do //membaca sampai end of file
@@ -117,7 +110,7 @@ implementation
         close(fileInput);
     end;
 
-    procedure loadBahanOlahan(var arrBahanOlahan : array of tipe.hasil_olah);
+    procedure loadBahanOlahan();
     //Mengambil list bahan olahan dari file 'listBahanOlahan.dat'
     //I.S: arrBahanOlahan kosong
     //F.S: arrBahanOlahan terisi
@@ -125,7 +118,7 @@ implementation
         loadResepTemplate(arrBahanOlahan, 'listBahanOlahan.dat')
     end;
 
-    procedure loadResep(var arrResep : array of tipe.hasil_olah); 
+    procedure loadResep(); 
     //Mengambil list list resep dari 'resep.dat'
     //I.S: arrResep kosong
     //F.S: arrResep terisi  
@@ -143,6 +136,7 @@ implementation
         idx: integer;
     
     begin
+        idx := 1;
         Assign(fileInput, FILENAME);
         reset(fileInput);
         while not eof(fileInput) do //Membaca sampai end of file
@@ -157,18 +151,50 @@ implementation
         end;
         close(fileInput);
     end;
-    procedure loadInventori(var arrInvMentah : array of tipe.isi_inventori; var arrInvOlahan : array of tipe.isi_inventori); 
+
+    procedure loadInventori(); 
     //Mengambil isi inventori bahan mentah dari 'listInvenMentah.dat' dan isi inventori
     //bahan olahan dari 'listInvenOlahan.dat'
     //I.S: arrInvMentah dan arrInvOlahan kosong
     //F.S: arrInvMentah dan arrInvOlahan terisi
+    begin
+        loadInventoriTemplate(arrInvMentah, 'listInvenMentah.dat');
+        loadInventoriTemplate(arrInvOlahan, 'listInvenOlahan.dat');
+    end;
+
+    procedure loadSimulasi();
+    //Mengambil data-data simulasi dari 'dataSimulasi.dat'
+    //I.S: array dataSimulasi kosong
+    //F.S: array dataSimulasi terisi
     var
         fileInput : text;
         parseRes: parseResult;
         idx: integer;
     
     begin
-        loadInventoriTemplate(arrInvMentah, 'listInvenMentah.dat');
-        loadInventoriTemplate(arrInvOlahan, 'listInvenOlahan.dat');
+        Assign(fileInput, 'dataSimulasi.dat');
+        reset(fileInput);
+        idx := 1;
+        while not eof(fileInput) do //Membaca sampai end of file
+        begin
+            parseRes := input(fileInput);
+            dataSimulasi[idx].nomor := convStringToInt(parseRes.arr[1]);
+            dataSimulasi[idx].hh_hidup := convStringToInt(parseRes.arr[2]);
+            dataSimulasi[idx].energi := convStringToInt(parseRes.arr[3]);
+            dataSimulasi[idx].inven_cap := convStringToInt(parseRes.arr[4]);
+            dataSimulasi[idx].mentah_dibeli := convStringToInt(parseRes.arr[5]);
+            dataSimulasi[idx].bhn_olah_dibuat := convStringToInt(parseRes.arr[6]);
+            dataSimulasi[idx].bhn_olah_dijual := convStringToInt(parseRes.arr[7]);
+            dataSimulasi[idx].resep_dijual := convStringToInt(parseRes.arr[8]);
+            dataSimulasi[idx].sum_income := convStringToInt(parseRes.arr[9]);
+            dataSimulasi[idx].sum_outcome := convStringToInt(parseRes.arr[10]);
+            dataSimulasi[idx].inven_used := convStringToInt(parseRes.arr[11]);
+            dataSimulasi[idx].sum_uang := convStringToInt(parseRes.arr[12]);
+            dataSimulasi[idx].dd := convStringToInt(parseRes.arr[13]);
+            dataSimulasi[idx].mm := convStringToInt(parseRes.arr[14]);
+            dataSimulasi[idx].yy := convStringToInt(parseRes.arr[15]);
+            idx += 1;
+        end;
+        close(fileInput);
     end;
 end.
--- tipe.pas
+++ tipe.pas
@@ -41,7 +41,19 @@ interface
                     mm : integer;
                     yy : integer;
                 end;
+                
+    var
+        arrBahanMentah : array [1..1024] of bahan_mentah;
+        arrBahanOlahan : array [1..1024] of hasil_olah;
+        dataSimulasi : array [1..128] of data;
+        arrInvMentah : array [1..512] of isi_inventori;
+        arrInvOlahan : array [1..512] of isi_inventori;
+        arrResep : array [1..256] of hasil_olah;
+        currentSimulasi : data;
+        uang : longint;
+        terminateProgram, simulasiRunning : boolean;
+        todayIstirahatCount, todayMakanCount : integer;
 
 implementation
 
-end.
+end;
