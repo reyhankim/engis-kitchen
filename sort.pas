054a06dc164edb4e88042654f3fc21f61da660e9
Firastic <adyaksa@students.itb.ac.id>
2018-04-18 13:54:06 +0700

    Add comment sort

--- sort.pas
+++ sort.pas
@@ -8,6 +8,9 @@ interface
 
 implementation
 	procedure sortBahanMentah(var arr: array of bahan_mentah);
+	//Mengurutkan array of bahan_mentah
+	//I.S: arr terdefinisi
+	//F.S: arr terurut secara alfabetik berdasarkan nama
 	var
 		i: integer;
 		sorted: boolean;
@@ -32,6 +35,9 @@ implementation
 		end;
 	end;
 	procedure sortHasilOlah(var arr: array of hasil_olah);
+	//Mengurutkan array of hasil_olah
+	//I.S: arr terdefinisi
+	//F.S: arr terurut secara alfabetik berdasarkan nama
 	var
 		i: integer;
 		sorted: boolean;
@@ -56,6 +62,9 @@ implementation
 		end;
 	end;
 	procedure sortIsiInventori(var arr: array of isi_inventori);
+	//Mengurutkan array of isi_inventori
+	//I.S: arr terdefinisi
+	//F.S: arr terurut secara alfabetik berdasarkan nama
 	var
 		i: integer;
 		sorted: boolean;
