a416911e2982b9fe081a29c46458c6b0ada22097
ArleyB <37997036+ArleyB@users.noreply.github.com>
2018-04-21 11:54:11 +0700

    Update main.pas

--- main.pas
+++ main.pas
@@ -6,11 +6,17 @@ uses tipe, handler, aksiLoad;
 begin
     //load seluruh data dari file eksternal
     loadSimulasi();
+    writeln('loadSimulasi OK');
     loadBahanMentah();
+    writeln('loadBahanMentah OK');
     loadBahanOlahan();
+    writeln('loadBahanOlahan OK');
     loadResep();
+    writeln('loadresep OK');
     terminateProgram := False; 
+    writeln('terminateProgram Initialized');
     while (not(terminateProgram)) do begin //jalankan program hingga exit()
         runUserInterface();
+        writeln('runUserInterface running');
     end;
 end.
